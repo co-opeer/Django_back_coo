ef9259370a2298298d15b7b026c03ed1caa71216
VlaBeh <166541377+VlaBeh@users.noreply.github.com>
2024-11-14 22:36:53 +0200

    add view

--- rusbery/views.py
+++ rusbery/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class Parking_SpotViewSet(viewsets.ModelViewSet):
+    queryset = Parking_Spot.objects.all()
+    serializer_class = Parking_Spot_Serializer
+
+
+class Parking_StatisticsViewSet(viewsets.ModelViewSet):
+    queryset = Parking_Statistics.objects.all()
+    serializer_class = Parking_Statistics_Serializer
+
+
+class Parking_LotViewSet(viewsets.ModelViewSet):
+    queryset = Parking_Lot.objects.all()
+    serializer_class = Parking_Lot_Serializer
